7295fad88e7ceb16abf7e48310b2b2c62aa13aa6
ISAMCS <workisam@outlook.com>
2025-01-17 18:46:50 -0600

    fixes

--- Project/frontend/app/login/page.js
+++ Project/frontend/app/login/page.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import LogoImg from '../public/Logo.png';
 import { useRouter } from 'next/navigation';
 
-const API_BASE_URL = 'https://memorymosaic.vercel.app/api';
+const API_BASE_URL = 'https://memorymosaic.vercel.app';
 
 const Login = () => {
   const [user, setUser] = useState(null);
